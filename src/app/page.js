59749a63d03a92c2714650823612d1ba2152c92f
MohsinGhani <Mohsinghani.777@gmail.com>
2024-11-25 19:21:12 +0500

    working on UI fixes

--- src/app/page.js
+++ src/app/page.js
@@ -6,11 +6,12 @@ import React from "react";
 import chart from "../../public/assets/images/Base Chart.png";
 import arrowIcon from "../../public/assets/images/arrow.png";
 import blueline from "../../public/assets/images/blue line.png";
-import WhyUs from "../components/homepage/whyUs";
-import ExperienceSection from "../components/homepage/experienceSection";
-import MakeUsUnique from "../components/homepage/uniqueSection";
-import HomepageCards from "../components/homepage/cards";
-import HomepageSlider from "../components/homepage/slider";
+import HomepageSlider from "@/components/homepage/slider";
+import WhyUs from "@/components/homepage/whyUs";
+import ExperienceSection from "@/components/homepage/experienceSection";
+import MakeUsUnique from "@/components/homepage/uniqueSection";
+import HomepageCards from "@/components/homepage/cards";
+
 function Home() {
   return (
     <div>
--- src/components/Homepage/WhyUs.js
+++ src/components/Homepage/WhyUs.js
@@ -3,13 +3,13 @@ import { CheckOutlined } from "@ant-design/icons";
 import chooseUs from "@/../public/assets/icons/Animation-about.gif";
 import CommonTypography from "../shared/Typography";
 
-export default function ExperiencePage() {
+export default function WhyUs() {
   const chooseList = [
     "Save time by eliminating the need to visit multiple platforms.",
     "Find reliable answers faster with consolidated search.",
     "Enhance your workflow with intuitive and interactive features.",
-    "Access the latest resources and solutions, all in one location."
-  ]
+    "Access the latest resources and solutions, all in one location.",
+  ];
   return (
     <div className="flex flex-col w-full xl:px-32 lg:px-4 px-9 md:py-9 items-center">
       <div className="md:flex justify-between w-full py-4">
@@ -17,9 +17,12 @@ export default function ExperiencePage() {
           <Image src={chooseUs} alt="Cube" width={600} />
         </div>
         <div className="md:w-2/4 flex justify-center flex-col gap-6">
-          <CommonTypography className="text-4xl font-bold text-[#191A15]">Why Choose Us?</CommonTypography>
+          <CommonTypography className="text-4xl font-bold text-[#191A15]">
+            Why Choose Us?
+          </CommonTypography>
           <CommonTypography className="text-lg text-[#565758]">
-            Get smarter, faster access to the best resources, all in one place, to streamline your workflow and find the right solutions quickly.
+            Get smarter, faster access to the best resources, all in one place,
+            to streamline your workflow and find the right solutions quickly.
           </CommonTypography>
           <ul className="mt-4 space-y-4 text-base text-[#565758]">
             {chooseList.map((text, index) => (
