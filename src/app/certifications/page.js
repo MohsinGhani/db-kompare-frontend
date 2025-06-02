51e2046b3351b69cc0be192b627058b9c29e0672
SameerAteeq <sameerateeq0@gmail.com>
2025-06-02 16:10:11 +0500

    certifications seo

--- src/app/certifications/page.js
+++ src/app/certifications/page.js
@@ -1,5 +1,16 @@
 import Certifications from "@/components/view/Certifications";
+import { generateCommonMetadata } from "@/utils/helper";
 import React from "react";
+
+
+const title = "Certifications";
+const description =
+  "Earn FREE certifications by completing quizzes on DB Kompare. Take quizzes at your own pace and showcase your skills with official certificates.";
+
+export const metadata = generateCommonMetadata({
+  title,
+  description,
+});
 
 const page = () => {
   return <Certifications />;
